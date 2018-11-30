07e94d5ada67d6d0505a18d826403bdd38e2c8d7
monochromer <monochromer@mail.ru>
2018-11-30 14:51:07 +0500

    Добавляет схему БД

---
+++ schema.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS `doingsdone`;
+
+CREATE DATABASE `doingsdone`
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+USE `doingsdone`;
+
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` CHAR(255) NOT NULL,
+  `email` CHAR(255) NOT NULL UNIQUE,
+  `password` CHAR(64) NOT NULL,
+  `register_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE INDEX `users_name_index` ON `users`(`name`);
+
+
+DROP TABLE IF EXISTS `projects`;
+
+CREATE TABLE `projects` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` CHAR(255) NOT NULL,
+  `author_id` INT(11) UNSIGNED REFERENCES `users`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE UNIQUE INDEX `projects_name_index` ON `projects`(`author_id`, `name`);
+
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `creation_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `completed_date` DATETIME,
+  `deadline` DATETIME,
+  `status` ENUM('0', '1') DEFAULT '0',
+  `name` TEXT NOT NULL,
+  `file` VARCHAR(1000),
+  `author_id` INT(11) UNSIGNED REFERENCES `users`(`id`) ,
+  `project_id` INT(11) UNSIGNED REFERENCES `projects`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE INDEX `tasks_deadline_index` ON `tasks`(`deadline`);
+CREATE INDEX `tasks_status_deadline_index` ON `tasks`(`status`, `deadline`);
+CREATE FULLTEXT INDEX `tasks_name_index` ON `tasks`(`name`);
